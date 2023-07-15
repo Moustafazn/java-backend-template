233c0c2a9b428ccc680892f7f6b42e99b55ee2a9
moustafazein <moustafazein@gmail.com>
2023-07-15 12:17:23 +0300

    Upgrade to spring boot 3.1.1 and security 7

--- src/main/java/io/task/template/config/SecurityConfig.java
+++ src/main/java/io/task/template/config/SecurityConfig.java
@@ -1,23 +1,29 @@
 package io.task.template.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain auth0FilterChain(final HttpSecurity http) throws Exception {
-        http.securityMatcher("/api/v1/**")
-                .authorizeRequests().anyRequest().permitAll()//.hasRole("sample-role")
-                .and()
-                .csrf(csrf -> csrf.disable())
-                // .httpBasic().disable()
+
+        http.authorizeHttpRequests(authz -> authz
+                .requestMatchers("/api/v1/**").permitAll()
+                //.hasRole("sample-role")
+                //.requestMatchers("/h2/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> {
                     httpSecuritySessionManagementConfigurer
                             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
